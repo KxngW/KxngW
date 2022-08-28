140de763940c3ca2da21970c06fb08869e1babe3
KxngW <111107817+KxngW@users.noreply.github.com>
2022-08-28 14:00:26 -0500

    Create v1.lua

---
+++ v1.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 9498006165 then -- Tapping Legends X
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KxngW/KxngW/main/KingdomHub.Lua"))()
+
+elseif game.PlaceId == 8540346411 then -- Rebirth Champions X
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KxngW/KxngW/main/RebirthChampX.lua"))()
+
+elseif game.PlaceId == 8884334497 then -- Mining Clicker Simulator
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KxngW/KxngW/main/MiningClickerSim.lua'))()
