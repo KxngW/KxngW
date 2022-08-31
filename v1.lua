87c4328bc44a40b4368ab18277c5af591483e431
KxngW <111107817+KxngW@users.noreply.github.com>
2022-08-30 23:59:00 -0500

    Update v1.lua

--- v1.lua
+++ v1.lua
@@ -1,11 +1,16 @@
+repeat
+    wait()
+until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game:IsLoaded()
+
 if game.PlaceId == 9498006165 then -- Tapping Simulator!
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KxngW/KxngW/main/KingdomHub.Lua"))()
 
 elseif game.PlaceId == 8884334497 then -- Mining Clicker Simulator
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KxngW/KxngW/main/MiningClickerSim.lua'))()
 
-elseif game.PlaceId = 7177496972 then -- Anime Warriors
+elseif game.PlaceId == 7177496972 then -- Anime Warriors
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KxngW/KxngW/main/AnimeWarriors.lua'))()
-    
-elseif game.PlaceId = 8540346411 then -- Rebirth Champions X
+
+elseif game.PlaceId == 8540346411 then -- Rebirth Champions X
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KxngW/KxngW/main/RebirthChampionsX.lua'))()
+end
