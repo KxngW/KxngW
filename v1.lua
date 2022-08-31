0f212e3ca8c02b431fcde07d6c2524d4bbf4b389
KxngW <111107817+KxngW@users.noreply.github.com>
2022-08-30 23:36:09 -0500

    Update v1.lua

--- v1.lua
+++ v1.lua
@@ -5,4 +5,7 @@ elseif game.PlaceId == 8884334497 then -- Mining Clicker Simulator
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KxngW/KxngW/main/MiningClickerSim.lua'))()
 
 elseif game.PlaceId = 7177496972 then -- Anime Warriors
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/KxngW/KxngW/main/AnimeWarriors.lua'))()
+    
+elseif game.PlaceId = 8540346411 then -- Rebirth Champions X
     loadstring(game:HttpGet('https://raw.githubusercontent.com/KxngW/KxngW/main/RebirthChampionsX.lua'))()
